dffb7f1b9500a8ca8c9840cea51c2aaf236f59ab
vbs13 <buanovv005@gmail.com>
2023-11-09 20:13:42 +0300

    ЕГЭ №5 дз выполнено

--- home_work/09.11.23/less1/main.py
+++ home_work/09.11.23/less1/main.py
@@ -1,1 +1,15 @@
-#TODO Код писать здесь.
+mas = []
+
+for n in range(0, 1001):
+    a = bin(n)[2:]
+    if a.count('1') % 2 == 0:
+        a += '0'
+        a = '10' + a[2:]
+    elif a.count('1') % 2 != 0:
+        a += '1'
+        a = '11' + a[2:]
+    r = int(a, 2)
+    if r > 40:
+        mas.append(n)
+
+print(min(mas))
