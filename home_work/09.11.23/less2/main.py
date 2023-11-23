9f963439a47f50e1ab646a7cd8a04211f3aa6900
vbs13 <buanovv005@gmail.com>
2023-11-23 18:33:03 +0300

    дз №2 - выполнено

--- home_work/09.11.23/less2/main.py
+++ home_work/09.11.23/less2/main.py
@@ -1,1 +1,11 @@
-#TODO Код писать здесь
+def nod(a, b):
+    x = min(a, b)
+    y = max(a, b)
+    for i in range(x, 0, -1):
+        if x % i == 0 and y % i == 0:
+            return i
+
+
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+print('Наибольший общий делитель:', nod(num1, num2))
