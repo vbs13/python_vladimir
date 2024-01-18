8d60c5ea1cb082eb4f05f8f4ca87bb2f4b70049b
vbs13 <buanovv005@gmail.com>
2024-01-18 18:58:51 +0300

    18/01 домашняя работа 09.11

--- home_work/09.11.23/less3/main.py
+++ home_work/09.11.23/less3/main.py
@@ -1,7 +1,24 @@
+# a = s[:len(s)//2]
+# if len(s) % 2 != 0:
+#     b = s[:len(s) // 2:-1]
+# else:
+#     b = s[:len(s) // 2 - 1:-1]
+# print(a, b)
+# if a == b:
+#     print('палиндром')
+# else:
+#     print('не палиндром')
+
+
+def palindrom(st):
+    for i in range(len(st)//2):
+        if st[i] != st[-(i + 1)]:
+            return False
+    return True
+
+
 s = input('Введите слово: ')
-for i in s:
-    if len(s) % 2 == 0:
-        if s[i] == s[i::-1]:
-            print('палиндром')
-        else:
-            print('не палиндром')
+if palindrom(s):
+    print('палиндром')
+else:
+    print('не палиндром')
